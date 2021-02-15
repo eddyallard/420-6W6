88a1579692d3e4f8a7268297d813412b200e8e05
Eddy <eallard1307@gmail.com>
2021-02-15 08:28:04 -0500

    Application properties config

---
+++ Cours3/src/test/java/qc/colval/demodbfirst/DemodbfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package qc.colval.demodbfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodbfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
