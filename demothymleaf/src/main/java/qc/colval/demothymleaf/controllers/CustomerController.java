9b3ba8b145d52a52ba4d8d9590831e77d28c8dd2
Eddy <eallard1307@gmail.com>
2021-03-08 10:27:32 -0500

    Fixed mapping

--- demothymleaf/src/main/java/qc/colval/demothymleaf/controllers/CustomerController.java
+++ demothymleaf/src/main/java/qc/colval/demothymleaf/controllers/CustomerController.java
@@ -1,9 +1,11 @@
 package qc.colval.demothymleaf.controllers;
 
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import qc.colval.demothymleaf.controllers.viewmodels.SearchNameViewModel;
 import qc.colval.demothymleaf.models.entities.Customer;
@@ -22,14 +24,14 @@ public class CustomerController {
 
     @GetMapping("/all")
     public String customer(Model model){
-        model.addAttribute("allCustomer", customerService.readAll().stream().limit(15).collect(Collectors.toList()));
+        model.addAttribute("searchNames", new SearchNameViewModel());
+        model.addAttribute("allCustomer", customerService.readAll().stream().limit(20).collect(Collectors.toList()));
         model.addAttribute("customerCount", customerService.readAll().size());
         return "customer/customer";
     }
 
     @PostMapping("/do_search_name")
     public String searchCustomerByName(Model model, SearchNameViewModel searchNameViewModel){
-        System.out.println(searchNameViewModel.getFirstname());
         List<Customer> customersWithName = customerService.getAllCustomerWithFirstNameSubStr(searchNameViewModel.getFirstname());
         int nbCustomers = customersWithName.size();
         model.addAttribute("searchNames", new SearchNameViewModel());
