73687c29bd774a70486e9c879789167d93682d3f
Eddy <eallard1307@gmail.com>
2021-04-01 11:36:26 -0400

    API

--- demothymleaf/src/main/java/qc/colval/demothymleaf/models/dto/CustomerDTO.java
+++ demothymleaf/src/main/java/qc/colval/demothymleaf/models/dto/CustomerDTO.java
@@ -3,9 +3,13 @@ package qc.colval.demothymleaf.models.dto;
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CustomerDTO {
 
     private Long customerId;
@@ -26,91 +30,4 @@ public class CustomerDTO {
     private Long addressId;
 
     private Long storeId;
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Date lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public Long getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(Long addressId) {
-        this.addressId = addressId;
-    }
-
-    public Long getStoreId() {
-        return storeId;
-    }
-
-    public void setStoreId(Long storeId) {
-        this.storeId = storeId;
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerDTO{" +
-                "customerId=" + customerId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", active=" + active +
-                ", createDate=" + createDate +
-                ", lastUpdate=" + lastUpdate +
-                ", addressId=" + addressId +
-                ", storeId=" + storeId +
-                '}';
-    }
 }
